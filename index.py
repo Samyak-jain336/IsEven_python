b9b8199f4dcded567f60fdfbfcba376713085a94
aHumanBeing036 <97162472+aHumanBeing036@users.noreply.github.com>
2022-01-19 18:56:11 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,21 @@
+def is_Even(number):
+    if number == 1:
+        return False
+    elif number == 2:
+        return True
+    elif number == 3:
+        return False
+    elif number == 4:
+        return True
+    elif number == 5:
+        return False
+    elif number == 6:
+        return True
+    elif number == 7:
+        return False
+    elif number == 8:
+        return True
+    elif number == 9:
+        return False
+    elif number == 10:
+        return True
